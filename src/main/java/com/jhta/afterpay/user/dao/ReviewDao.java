104a506a314c6edfbf7c18d3c41006a39b05af74
2eunjung <12103207@iscu.ac.kr>
2024-09-19 11:40:51 +0900

    ReviewDao 메소드 반환타입 변경하여 오류 해결
    추가로 사용자가 삭제하지 않은 리뷰 목록 조회하는 메소드 추가할 예정

--- src/main/java/com/jhta/afterpay/user/dao/ReviewDao.java
+++ src/main/java/com/jhta/afterpay/user/dao/ReviewDao.java
@@ -6,30 +6,40 @@ import com.jhta.afterpay.user.vo.User;
 import com.jhta.afterpay.util.DaoHelper;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ReviewDao {
 
     /**
      * 사용자번호로 작성한 리뷰 중 삭제하지 않은 리뷰의 총 갯수 조회
-     * @param userNo
-     * @return
-     * @throws SQLException
+     * @param userNo 사용자번호
+     * @return 리뷰 갯수
      */
-    public Review getReviewCntByUserNo(int userNo) throws SQLException {
+    public int getReviewCntByUserNo(int userNo) {
         String sql = """
-                SELECT COUNT(USER_NO)
+                SELECT COUNT(USER_NO) cnt
                 FROM REVIEWS
                 WHERE USER_NO = ?
                     AND ISDELETED = 'N'
                 """;
 
-        return DaoHelper.selectOneInt(sql, rs -> {
-            Review review = new Review();
-            User user = new User();
-            user.setNo(rs.getInt("user_no"));
-            review.setUserNo(user);
-            return review;
-        }, userNo);
+        return DaoHelper.selectOneInt(sql, userNo);
     }
+
+    /**
+     * 사용자가 삭제하지 않은 리뷰들 목록 조회
+     * @param userNo
+     * @return
+     */
+//    public List<Review> getReviewsByUserNo(int userNo) {
+//        String sql = """
+//                SELECT REVIEW_TITLE
+//                    , REVIEW_CONTENT
+//                    , REVIEW_CREATED_DATE
+//                FROM REVIEWS
+//                WHERE USER_NO = ?
+//                    AND ISDELETED = 'N'
+//                """;
+//    }
 }

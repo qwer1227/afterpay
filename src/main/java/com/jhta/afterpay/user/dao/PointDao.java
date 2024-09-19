6d5d76c64273e1262aa36a30242c55a4032af1e1
2eunjung <12103207@iscu.ac.kr>
2024-09-19 11:41:45 +0900

    PointDao 적립금 내역 조회 메소드 보완

--- src/main/java/com/jhta/afterpay/user/dao/PointDao.java
+++ src/main/java/com/jhta/afterpay/user/dao/PointDao.java
@@ -4,19 +4,34 @@ import com.jhta.afterpay.user.vo.Point;
 import com.jhta.afterpay.util.DaoHelper;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class PointDao {
 
-    public Point getPointByUserNo(int userNo) throws SQLException{
+    /**
+     * 사용자번호를 전달받아 적립금 내역 조회
+     * @param userNo
+     * @return
+     */
+    public List<Point> getPointHistoriesByUserNo(int userNo) {
         String sql = """
-                SELECT HISTORY_NO
-                    , HISTORY_DATE
-                    , HISTORY_CONTENT
-                    , HISTORY_POINT
-                FROM POINT_HISTORIES
-                WHERE USER_NO = ?
+                SELECT U.POINT
+                    , U.TOTAL_POINT
+                    , U.TOTAL_USED_POINT
+                    , H.HISTORY_DATE
+                    , H.HISTORY_CONTENT
+                FROM USERS U JOIN POINT_HISTORIES H
+                    ON U.USER_NO = H.USER_NO
+                WHERE U.USER_NO = ?
                 """;
-
-            return DaoHelper.selectOne()
+        return DaoHelper.selectList(sql, rs -> {
+            Point point = new Point();
+            point.setPoint(rs.getInt("u.point"));
+            point.setTotalPoint(rs.getInt("u.total_point"));
+            point.setUsedPoint(rs.getInt("u.total_used_point"));
+            point.setHistoryDate(rs.getDate("h.history_date"));
+            point.setContent(rs.getString("h.history_content"));
+            return point;
+        }, userNo);
     }
 }

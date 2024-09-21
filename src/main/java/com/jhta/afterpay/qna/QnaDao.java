f5c3777cd31af8306855795baa8ee0c4e78bdfda
2eunjung <12103207@iscu.ac.kr>
2024-09-21 21:11:11 +0900

    QnaDao 클래스 폴더 이동

--- src/main/java/com/jhta/afterpay/qna/QnaDao.java
+++ src/main/java/com/jhta/afterpay/qna/QnaDao.java
@@ -61,4 +61,41 @@ public class QnaDao {
             return qna;
         }, begin, end);
     }
+
+    /**
+     * 사용자가 삭제하지 않은 문의 게시글 목록 조회
+     * @param userNo
+     * @return
+     */
+    public List<Qna> getQnaByUserNo(int userNo) {
+        String sql = """
+                SELECT *
+                FROM QNAS
+                WHERE USER_NO = ?
+                    AND ISQNADELETED = 'N'
+                ORDER BY QNA_CREATED_DATE DESC
+                """;
+
+        return DaoHelper.selectList(sql, rs -> {
+            Qna qna = new Qna();
+
+            qna.setNo(rs.getInt("qna_no"));
+            qna.setTitle(rs.getString("qna_title"));
+            qna.setContent(rs.getString("qna_content"));
+            qna.setCnt(rs.getInt("qna_cnt"));
+            qna.setCreatedDate(rs.getDate("qna_created_date"));
+            qna.setRepliedDate(rs.getDate("qna_replied_date"));
+            qna.setRepliedContent(rs.getString("qna_replied_content"));
+            qna.setIsQnaDeleted(rs.getString("isqnadeleted"));
+
+
+            User user = new User();
+            user.setNo(rs.getInt("user_no"));
+            qna.setUser(user);
+
+            return qna;
+        }, userNo);
+    }
+
+
 }

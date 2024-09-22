f0eaad109074fd7bcf0b87a28ed4b5f93633bdfa
2eunjung <12103207@iscu.ac.kr>
2024-09-22 18:53:33 +0900

    Update ReviewDao.java

--- src/main/java/com/jhta/afterpay/user/ReviewDao.java
+++ src/main/java/com/jhta/afterpay/user/ReviewDao.java
@@ -32,6 +32,7 @@ public class ReviewDao {
                 SELECT REVIEW_TITLE
                     , REVIEW_CONTENT
                     , REVIEW_CREATED_DATE
+                    , REVIEW_RATING
                 FROM REVIEWS
                 WHERE USER_NO = ?
                     AND ISDELETED = 'N'
@@ -44,8 +45,56 @@ public class ReviewDao {
             review.setTitle(rs.getString("REVIEW_TITLE"));
             review.setContent(rs.getString("REVIEW_CONTENT"));
             review.setCreatedDate(rs.getDate("REVIEW_CREATED_DATE"));
+            review.setRating(rs.getInt("REVIEW_RATING"));
 
             return review;
         }, userNo);
+    }
+
+    public int getAllTotalRowsByUserNo(int userNo) {
+        String sql = """
+                SELECT COUNT(*)
+                FROM REVIEWS
+                WHERE USER_NO = ?
+                """;
+
+        return DaoHelper.selectOneInt(sql, userNo);
+    }
+
+    public List<Review> getAllReviewByUserNo(int begin, int end) {
+        String sql = """
+                SELECT ROW_NUMBER() OVER (ORDER BY REVIEW_NO DESC) ROWNUMBER
+                        , REVIEW_NO
+                        , REVIEW_CONTENT
+                        , REVIEW_CREATED_DATE
+                        , REVIEW_TITLE
+                        , ISDELETED
+                        , REVIEW_RATING
+                        , PRODUCT_NO
+                FROM REVIEWS
+                WHERE ROWNUMBER BETWEEN ? AND ?
+                    AND USER_NO = ?
+                """;
+
+        return DaoHelper.selectList(sql, rs -> {
+            Review review = new Review();
+            review.setNo(rs.getInt("review_no"));
+            review.setTitle(rs.getString("review_title"));
+            review.setContent(rs.getString("review_content"));
+            review.setCreatedDate(rs.getDate("review_created_date"));
+            review.setProductNo(rs.getInt("product_no"));
+            review.setIsDeleted(rs.getString("isdeleted"));
+            review.setRating(rs.getInt("review_rating"));
+            return review;
+        }, begin, end);
+    }
+
+    public void deleteReviewByReviewNo(int reviewNo) {
+        String sql = """
+                UPDATE REVIEWS
+                SET ISDELETED = 'Y'
+                WHERE REVIEW_NO = ?
+                """;
+        DaoHelper.update(sql, reviewNo);
     }
 }
